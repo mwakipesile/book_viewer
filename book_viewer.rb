79848873414b64a2e8ce10eda795a2fe31f34466
M. Edgar joel <edgar-@users.noreply.github.com>
2016-09-10 09:45:36 -0400

    Added chapters

--- book_viewer.rb
+++ book_viewer.rb
@@ -3,5 +3,9 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+  # Alt: File.readlines('data/toc.txt') to array,
+  # then use 'each' instead of 'each_line' in views/home.erb
+  @contents = File.read('data/toc.txt') 
   erb :home
 end
