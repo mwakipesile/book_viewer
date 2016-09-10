1601a47c599b09576b25bb2fa0c2ee3792c17d97
M. Edgar joel <mkakajoel@gmail.com>
2016-09-10 13:55:09 -0400

    Added Chapter 1

--- book_viewer.rb
+++ book_viewer.rb
@@ -6,12 +6,15 @@ get "/" do
   @title = "The Adventures of Sherlock Holmes"
   # Alt: File.readlines('data/toc.txt') to array,
   # then use 'each' instead of 'each_line' in views/home.erb
+  @author = "Sir Arthur Doyle"
   @contents = File.read('data/toc.txt') 
   erb :home
 end
 
-get "/chapters" do
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
 
-  @contents = File.read('data/toc.txt') 
   erb :chapter
 end
