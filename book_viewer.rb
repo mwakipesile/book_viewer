6b96f5970e8b08de21693071a7148bd18b321eb4
M. Edgar joel <mkakajoel@gmail.com>
2016-09-12 14:43:19 -0400

    Refactoring

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,19 +2,21 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @heading = 'Table of Contents'
+  @table_of_contents = File.readlines("data/toc.txt")
+  @author = "Sir Arthur Doyle"
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  # Alt: File.readlines('data/toc.txt') to array,
-  # then use 'each' instead of 'each_line' in views/home.erb
-  @author = "Sir Arthur Doyle"
-  @contents = File.read('data/toc.txt') 
   erb :home
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  chapter_number = params[:number].to_i
+  @title = "Chapter #{chapter_number}: #{@table_of_contents[chapter_number - 1]}"
+  @chapter = File.read("data/chp#{chapter_number}.txt")
 
   erb :chapter
 end
