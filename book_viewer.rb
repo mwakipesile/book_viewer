71438ef8ef34c4c078c67aa3646e3500651b6a9d
M. Edgar joel <mkakajoel@gmail.com>
2016-09-13 00:16:55 -0400

    Added search functionality

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,10 +2,21 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+helpers do
+  def in_paragraphs(text)     
+    # text.split("\n\n").map { |paragraph| "<p>#{paragraph}</p>" }.join("\n\n")
+    "<p>#{text.gsub("\n\n", "</p>\n\n<p>")}</p>"
+  end
+end
+
 before do
   @heading = 'Table of Contents'
   @table_of_contents = File.readlines("data/toc.txt")
   @author = "Sir Arthur Doyle"
+end
+
+not_found do
+  redirect '/'
 end
 
 get "/" do
@@ -13,10 +24,39 @@ get "/" do
   erb :home
 end
 
-get "/chapters/:number" do
-  chapter_number = params[:number].to_i
-  @title = "Chapter #{chapter_number}: #{@table_of_contents[chapter_number - 1]}"
-  @chapter = File.read("data/chp#{chapter_number}.txt")
+get "/chapters/:number" do |chapter_num|
+
+  begin
+    chapter = File.read("data/chp#{chapter_num}.txt")
+  rescue
+    redirect('/')
+  end
+
+  @title = "Chapter #{chapter_num}: #{@table_of_contents[chapter_num.to_i - 1]}"
+  @chapter = in_paragraphs(chapter)
 
   erb :chapter
+end
+
+get "/search" do
+  @search_str = params['query']
+  @results = []
+
+  if @search_str
+    found_indices = []
+    @chapter_count = @table_of_contents.size
+    @table_of_contents.each.with_index do |title, idx|
+      found_indices << idx if title.match(/#{@search_str}/i)
+    end
+
+    @chapter_count.times do |idx|
+      next if found_indices.include?(idx)
+
+      chapter = File.read("data/chp#{idx + 1}.txt")
+      found_indices << idx if chapter.match(/#{@search_str}/i)
+    end
+
+    @results = found_indices.map { |idx| @table_of_contents[idx]}
+  end
+  erb :search
 end
